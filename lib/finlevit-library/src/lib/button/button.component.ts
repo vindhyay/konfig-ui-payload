9653324038aab1d37e1913e4d01f768c462d62d3
vijay <vmotupalli@finlevit.com>
2021-08-31 14:39:31 -0400

    added support to change color and back ground color of the buttons

--- lib/finlevit-library/src/lib/button/button.component.ts
+++ lib/finlevit-library/src/lib/button/button.component.ts
@@ -1,56 +1,58 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 export enum ButtonVariants {
-  raisedButton = 'raisedButton',
-  roundedButton = 'roundedButton',
-  raisedTextButton = 'raisedTextButton',
-  outlinedButton = 'outlinedButton'
+  raisedButton = "raisedButton",
+  roundedButton = "roundedButton",
+  raisedTextButton = "raisedTextButton",
+  outlinedButton = "outlinedButton"
 }
 
 export enum ButtonTypes {
-  primary = 'primary',
-  info = 'info',
-  success = 'success',
-  secondary = 'secondary'
+  primary = "primary",
+  info = "info",
+  success = "success",
+  secondary = "secondary"
 }
 
 const ButtonClass = {
-  raisedButton: 'p-button-raised',
-  roundedButton: 'p-button-rounded',
-  raisedTextButton: 'p-button-raised p-button-text',
-  outlinedButton: 'p-button-outlined',
-  primary: 'p-button-primary',
-  info: 'p-button-info',
-  success: 'p-button-success',
-  secondary: 'p-button-secondary'
+  raisedButton: "p-button-raised",
+  roundedButton: "p-button-rounded",
+  raisedTextButton: "p-button-raised p-button-text",
+  outlinedButton: "p-button-outlined",
+  primary: "p-button-primary",
+  info: "p-button-info",
+  success: "p-button-success",
+  secondary: "p-button-secondary"
 };
 
 @Component({
-  selector: 'finlevit-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "finlevit-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"]
 })
 export class ButtonComponent {
   @Input() variant: ButtonVariants = ButtonVariants.raisedButton;
   @Input() type: ButtonTypes = ButtonTypes.primary;
   @Input() isSmall = false;
   @Input() isLarge = false;
+  @Input() color = "#fff";
+  @Input() bgColor = "#007bff";
   @Input() isDisabled = false;
-  @Input() label = '';
-  @Input() icon = '';
-  @Input() iconPos = 'left';
+  @Input() label = "";
+  @Input() icon = "";
+  @Input() iconPos = "left";
   @Input() info = false;
   @Input() secondary = false;
   @Input() success = false;
   @Output() btnClick = new EventEmitter();
-  private _class = '';
+  private _class = "";
   get class(): string {
-    this._class = ButtonClass[this.variant] + ' ' + ButtonClass[this.type];
+    this._class = ButtonClass[this.variant] + " " + ButtonClass[this.type];
     if (this.isSmall) {
-      this._class = this._class + ' p-button-sm';
+      this._class = this._class + " p-button-sm";
     }
     if (this.isLarge) {
-      this._class = this._class + ' p-button-lg';
+      this._class = this._class + " p-button-lg";
     }
     return this._class;
   }
