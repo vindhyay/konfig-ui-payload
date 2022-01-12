f8d4f588d9e4b8c5301cb83239d467e3cbe5e4f5
Mourya Andey <mandey@finlevit.com>
2022-01-12 17:02:47 +0530

    removed mat modules except menu

--- lib/finlevit-library/src/lib/finlevit-library.module.ts
+++ lib/finlevit-library/src/lib/finlevit-library.module.ts
@@ -1,47 +1,46 @@
-import { NgModule } from '@angular/core';
-import { FinlevitLibraryComponent } from './finlevit-library.component';
-import { InputComponent } from './input/input.component';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputMaskModule } from 'primeng/inputmask';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CardModule } from 'primeng/card';
-import { HttpClientModule } from '@angular/common/http';
-import { CardComponent } from './card/card.component';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { DropdownModule } from 'primeng/dropdown';
-import { TooltipModule } from 'primeng/tooltip';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { TabsComponent } from './tabs/tabs.component';
-import { NumberComponent } from './number/number.component';
-import { CheckboxComponent } from './checkbox/checkbox.component';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CheckboxModule } from 'primeng/checkbox';
-import { TextareaComponent } from './textarea/textarea.component';
-import { SelectComponent } from './select/select.component';
-import { MultiSelectComponent } from './multi-select/multi-select.component';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { RadioGroupComponent } from './radio-group/radio-group.component';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { CheckboxGroupComponent } from './checkbox-group/checkbox-group.component';
-import { ButtonComponent } from './button/button.component';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-import { DatepickerComponent } from './datepicker/datepicker.component';
-import { CalendarModule } from 'primeng/calendar';
-import {TableModule} from "primeng/table";
-import { NgxMaskModule } from 'ngx-mask';
+import { NgModule } from "@angular/core";
+import { FinlevitLibraryComponent } from "./finlevit-library.component";
+import { InputComponent } from "./input/input.component";
+import { InputTextModule } from "primeng/inputtext";
+import { InputMaskModule } from "primeng/inputmask";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CardModule } from "primeng/card";
+import { HttpClientModule } from "@angular/common/http";
+import { CardComponent } from "./card/card.component";
+import { InputNumberModule } from "primeng/inputnumber";
+import { DropdownModule } from "primeng/dropdown";
+import { TooltipModule } from "primeng/tooltip";
+import { TabMenuModule } from "primeng/tabmenu";
+import { TabsComponent } from "./tabs/tabs.component";
+import { NumberComponent } from "./number/number.component";
+import { CheckboxComponent } from "./checkbox/checkbox.component";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { CheckboxModule } from "primeng/checkbox";
+import { TextareaComponent } from "./textarea/textarea.component";
+import { SelectComponent } from "./select/select.component";
+import { MultiSelectComponent } from "./multi-select/multi-select.component";
+import { MultiSelectModule } from "primeng/multiselect";
+import { RadioGroupComponent } from "./radio-group/radio-group.component";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { CheckboxGroupComponent } from "./checkbox-group/checkbox-group.component";
+import { ButtonComponent } from "./button/button.component";
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+import { DatepickerComponent } from "./datepicker/datepicker.component";
+import { CalendarModule } from "primeng/calendar";
+import { TableModule } from "primeng/table";
+import { NgxMaskModule } from "ngx-mask";
 
-import { TableComponent } from './table/table.component';
+import { TableComponent } from "./table/table.component";
 import { FinlevitFileUploadComponent } from "./finlevit-file-upload/finlevit-file-upload.component";
 import { LoaderComponent } from "./loader/loader.component";
 import { NgxLoadingModule } from "ngx-loading";
-import {BackButtonComponent} from "./back-button/back-button.component";
-import {MenuButtonComponent} from "./menu-button/menu-button.component";
-import {OptionsButtonComponent} from "./options-button/options-button.component";
-import {_MatMenuDirectivesModule, MatMenuModule} from "@angular/material/menu";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatButtonModule} from "@angular/material/button";
+import { BackButtonComponent } from "./back-button/back-button.component";
+import { MenuButtonComponent } from "./menu-button/menu-button.component";
+import { OptionsButtonComponent } from "./options-button/options-button.component";
+import { MatMenuModule } from "@angular/material/menu";
+import { DividerModule } from "primeng/divider";
 
 @NgModule({
   declarations: [
@@ -87,10 +86,8 @@ import {MatButtonModule} from "@angular/material/button";
     TableModule,
     NgxMaskModule.forRoot({}),
     NgxLoadingModule,
-    _MatMenuDirectivesModule,
-    MatButtonModule,
     MatMenuModule,
-    MatDividerModule
+    DividerModule
   ],
   exports: [
     FinlevitLibraryComponent,
