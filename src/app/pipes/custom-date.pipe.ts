93dee8c01f96b2ec64553e603370fe79548ed0a6
Mourya Andey <mandey@finlevit.com>
2022-03-23 22:32:10 +0530

    modified custom date pipe

--- src/app/pipes/custom-date.pipe.ts
+++ src/app/pipes/custom-date.pipe.ts
@@ -8,6 +8,15 @@ export class CustomDatePipe implements PipeTransform {
   transform(date: Date | string, format: string = "medium"): string {
     date = new Date(date);
     date.setDate(date.getDate());
+    if(format.includes('mm')){
+      format = format.replace('mm', 'MM')
+    }
+    if(format === 'MM yy'){
+      format = 'MMMM y'
+    }
+    if(format === 'DD MM yy'){
+      format = 'EEEE MMMM y'
+    }
     return new DatePipe("en-US").transform(date, format);
   }
 }
