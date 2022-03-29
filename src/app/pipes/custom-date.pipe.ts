fdc096c6969d032560749d2c4691a5df41b5706d
vijay <vmotupalli@finlevit.com>
2022-03-29 11:16:26 -0400

    invalid date pipe issue fix

--- src/app/pipes/custom-date.pipe.ts
+++ src/app/pipes/custom-date.pipe.ts
@@ -6,16 +6,19 @@ import { DatePipe } from "@angular/common";
 })
 export class CustomDatePipe implements PipeTransform {
   transform(date: Date | string, format: string = "medium"): string {
+    if (!date) {
+      return "";
+    }
     date = new Date(date);
     date.setDate(date.getDate());
-    if(format.includes('mm')){
-      format = format.replace('mm', 'MM')
+    if (format.includes("mm")) {
+      format = format.replace("mm", "MM");
     }
-    if(format === 'MM yy'){
-      format = 'MMMM y'
+    if (format === "MM yy") {
+      format = "MMMM y";
     }
-    if(format === 'DD MM yy'){
-      format = 'EEEE MMMM y'
+    if (format === "DD MM yy") {
+      format = "EEEE MMMM y";
     }
     return new DatePipe("en-US").transform(date, format);
   }
