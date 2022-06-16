0d11953e7c54eb0f274f04ed9988d202cfa4844a
vijay <vmotupalli@finlevit.com>
2022-06-16 11:57:28 -0400

    table column hide and resize optimisation

---
+++ src/app/pipes/custom-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { WidgetTypeFilterPipe } from "./custom-filter.pipe";
+
+describe("WidgetTypeFilterPipe", () => {
+  it("create an instance", () => {
+    const pipe = new WidgetTypeFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
