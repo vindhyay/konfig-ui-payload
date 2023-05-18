3728f442ff310d518f58a53c14ee8d470c067572
dbhargav <dbhargav@stealthpath.com>
2023-05-18 19:33:52 +0530

    fix for page errors and page warning

--- src/app/modules/task/editor.service.ts
+++ src/app/modules/task/editor.service.ts
@@ -407,14 +407,19 @@ export class EditorService extends BaseService {
             if (data?.payload) {
               transactionDetails.uiPayload = data?.payload;
               this.setTransactionDetails(transactionDetails);
+              if (showHideIds?.length) {
+                transactionDetails.uiPayload.forEach(widget => {
+                  this.widgetChange.next(widget);
+                }); 
+              }
             }
             if (data?.errors && data?.errors?.length) {
               data?.errors.forEach(error => {
-                switch (error?.errorType) {
-                  case 'error':
+                switch (error?.type) {
+                  case 'PAGE_ERROR':
                     this.notificationService.error(error?.message);
                     break;
-                  case 'warning':
+                  case 'PAGE_WARNING':
                     this.notificationService.info(error?.message);
                     break;
                 }
